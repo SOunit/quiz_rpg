a1154da561e197ac54fde466ec03567d7f8ea2f4
SOunit <workbookwork7@gmail.com>
2021-10-13 14:07:52 -0700

    add hpbar animation

--- src/components/battle/hpBar/HpBar.js
+++ src/components/battle/hpBar/HpBar.js
@@ -1,9 +1,17 @@
 import classes from './HpBar.module.css';
 
 const HpBar = (props) => {
+  const test = ((props.currentHp / props.maxHp) * 100).toFixed(0);
+
   return (
     <div className={classes['hp-bar']}>
-      {props.currentHp} / {props.maxHp}
+      <p className={classes['hp-bar__text']}>
+        {props.currentHp} / {props.maxHp}
+      </p>
+      <div
+        style={{ width: test + '%' }}
+        className={classes['hp-bar__fill']}
+      ></div>
     </div>
   );
 };
